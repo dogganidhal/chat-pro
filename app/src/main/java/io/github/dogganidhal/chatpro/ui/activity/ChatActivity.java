f3cd4531272ee9f5ebd2bbfd6aab7f9c2d8a1c96
DOGGA Nidhal <dogga.nidhal@gmail.com>
2019-04-09 15:45:31 +0200

    Layout the chat message input

---
+++ app/src/main/java/io/github/dogganidhal/chatpro/ui/activity/ChatActivity.java
@@ -0,0 +1,15 @@
+package io.github.dogganidhal.chatpro.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import io.github.dogganidhal.chatpro.R;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_chat);
+  }
+}
